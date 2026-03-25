5316621af23c0de535b4702022f013bc910d455f
langyan <langyan.lin@gmail.com>
2026-03-25 15:06:27 +0800

    lin-spring-microservices

---
+++ lin-spring-microservices/lin-spring-service-order/src/main/java/com/lin/spring/service/order/dto/OrderItemRequest.java
@@ -0,0 +1,30 @@
+package com.lin.spring.service.order.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemRequest {
+
+    @NotBlank(message = "Product code is required")
+    private String productCode;
+
+    @NotBlank(message = "Product name is required")
+    private String productName;
+
+    @NotNull(message = "Quantity is required")
+    @Positive(message = "Quantity must be positive")
+    private Integer quantity;
+
+    @NotNull(message = "Price is required")
+    @Positive(message = "Price must be positive")
+    private BigDecimal price;
+}
